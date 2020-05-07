a9734e3a777ba533a271e57b1f72f1fb2adaa0b1
mvrlrd <igrbelykh@gmail.com>
2020-05-07 11:10:59 +0300

    edit text, snack bar, bottom sheet

--- app/src/main/java/ru/mvrlrd/materialdesign/MainActivity.java
+++ app/src/main/java/ru/mvrlrd/materialdesign/MainActivity.java
@@ -1,14 +1,77 @@
 package ru.mvrlrd.materialdesign;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    EditText editText;
+    Button button;
+    TextView textBottom2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+        textBottom2 = findViewById(R.id.bottom_sheet_2);
+
+
+        // get the bottom sheet view
+        LinearLayout llBottomSheet = findViewById(R.id.bottom_sheet);
+
+        // init the bottom sheet behavior
+        BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(llBottomSheet);
+
+        // change the state of the bottom sheet
+//        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+//        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+
+        // set the peek height
+        bottomSheetBehavior.setPeekHeight(150);
+
+        // set hideable or not
+//        bottomSheetBehavior.setHideable(true);
+
+        // set callback for changes
+        bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                Log.d(TAG, "onStateChanged: ");
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+                Log.d(TAG, "onSlide: ");
+            }
+        });
+    }
+
+
+    public void onClickButton(View view) {
+        //Toast.makeText(this, "mes", Toast.LENGTH_SHORT).show();
+        textBottom2.setText(editText.getText());
+        Snackbar snackbar = Snackbar.make(view, editText.getText(), Snackbar.LENGTH_LONG);
+        snackbar.setAction("Change to Moscow", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText("Moscow");
+                textBottom2.setText(editText.getText());
+            }
+        });
+        snackbar.show();
     }
 }
